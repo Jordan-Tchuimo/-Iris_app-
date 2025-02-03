6873b9fe3fc9d281693b218f79995d137e5bb0e2
Jordan-Tchuimo <jorditchuimo@gmail.com>
2025-02-03 15:01:50 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,1 +1,20 @@
+import streamlit as st
+import requests
+
+# Saisir les données d'entrée
+sepal_length = st.number_input('Longueur du sépale', min_value=0.0, max_value=10.0, value=5.0)
+sepal_width = st.number_input('Largeur du sépale', min_value=0.0, max_value=10.0, value=3.0)
+petal_length = st.number_input('Longueur du pétale', min_value=0.0, max_value=10.0, value=3.0)
+petal_width = st.number_input('Largeur du pétale', min_value=0.0, max_value=10.0, value=1.0)
+
+# Appeler l'API Flask
+if st.button('Prédire'):
+    response = requests.post('http://localhost:5000/predict', json={
+        'sepal_length': sepal_length,
+        'sepal_width': sepal_width,
+        'petal_length': petal_length,
+        'petal_width': petal_width
+    })
+    prediction = response.json()['prediction']
+    st.write(f"La prédiction pour cette fleur est : {prediction}")
 
