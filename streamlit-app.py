f176b089e3af5fc2c54229216a5d6520d508bf1b
Jordan-Tchuimo <jorditchuimo@gmail.com>
2025-02-17 00:42:49 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -7,3 +7,14 @@ st.text("Built with streamlit")
 #Headers and subheader
 st.header("EDA App")
 st.subheader("Iris Dataset")
+
+
+#Checkbox
+if st.checkbox("Show Dataset"):
+  st.text("Showing Dataset")
+
+#Radio Buttons
+gender = st.radio("what is your gender?",("Male","female"))
+if gender == 'Male':
+  st.text("Hello Guy")
+if gender==
