cccc93e70783d76cf92fb7e9eab67a564814520b
Jordan-Tchuimo <jorditchuimo@gmail.com>
2025-02-17 00:22:15 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,26 +1,9 @@
 import streamlit as st
-import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
 
-# Charger le jeu de données Iris
-iris = load_iris()
-df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
-df['species'] = iris.target_names[iris.target]
+#title/text
+st.title("IRIS EDA App")
+st.text("Built with streamlit")
 
-# Titre de l'application
-st.title('Analyse du jeu de données Iris')
-
-# Afficher les premières lignes du jeu de données
-st.subheader('Voici les premières lignes du jeu de données Iris:')
-st.write(df.head())
-
-# Visualisation interactive avec Seaborn
-st.subheader('Visualisation des relations entre les variables')
-sns.pairplot(df, hue="species")
-st.pyplot()
-
-# Afficher des statistiques descriptives
-st.subheader('Statistiques descriptives du jeu de données Iris:')
-st.write(df.describe())
+#Headers and subheader
+st.header("EDA App")
+st.subheader("Iris Dataset")
