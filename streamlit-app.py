75349d7de84cf886d6a6c8c28587a3c6b17e9b50
Jordan-Tchuimo <jorditchuimo@gmail.com>
2025-02-17 19:04:05 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,140 +1,34 @@
 import streamlit as st
-#EDA
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
-from PIL import Image, ImageEnhance,ImageFiltrer
+import requests
 
+# Créer l'interface utilisateur
+st.title("Prédiction de l'Espèce d'Iris")
 
-#title/text
-st.title("Iris EDA App")
-st.text("Built with streamlit")
+# Formulaire pour saisir les caractéristiques de l'iris
+sepal_length = st.number_input('Longueur du sépale (cm)', min_value=0.0, max_value=10.0, step=0.1)
+sepal_width = st.number_input('Largeur du sépale (cm)', min_value=0.0, max_value=10.0, step=0.1)
+petal_length = st.number_input('Longueur du pétale (cm)', min_value=0.0, max_value=10.0, step=0.1)
+petal_width = st.number_input('Largeur du pétale (cm)', min_value=0.0, max_value=10.0, step=0.1)
 
-#EDA
-my_dataset = 'Iris.csv'
+# Créer un bouton pour envoyer la requête
+if st.button('Faire une Prédiction'):
+    # Organiser les données dans un dictionnaire
+    data = {
+        'sepal_length': sepal_length,
+        'sepal_width': sepal_width,
+        'petal_length': petal_length,
+        'petal_width': petal_width
+    }
 
-#Fxn to Load Dataset
-@st.cache(persist=True)
-def explore_data(dataset):
-  df = pd.read_csv("Iris.csv")  # Example, check this
-  return df
+    # Faire une requête POST à l'API Flask
+    response = requests.post("http://127.0.0.1:5000/predict", json=data)
 
+    # Afficher la prédiction
+    if response.status_code == 200:
+        prediction = response.json()['species']
+        st.write(f"La prédiction est : {prediction}")
+    else:
+        st.write("Erreur dans la requête")
 
-
-if st.checkbox ("Preview Dataset"):
-  data = explore_data(my_dataset)
-  if st.button("Head"):
-    st.write(data.head())
-  elif st.button("Tail"):
-    st.write(data.tail())
-  else:
-    st.write(data.head(2))
-
-#Show entire dataset
-if st.checkbox("Show All Dataset"):
-  #st.write(data)
-  st.dataframe(data)
-
-#Show Column Name
-if st.checkbox("Show Column Names"):
-  st.write(data.columns)
-
-
-#Show Dimensions
-data_dim= st.radio("What Dimensions Do you want to see?",("Rows","Columns","All"))
-if data_dim == 'Rows':
-  st.text("Showing Rows")
-  st.write(data.shape[0])
-elif data_dim == 'Columns':
-  st.text("Showing Columns")
-  st.write(data.shape[1])
-else:
-  st.text("Showing Shape of Dataset")
-  st.write(data.shape)
-
-
-#Sumary
-if st.checkbox("Show Sumary of Dataset"):
-  st.write(data.describe())
-
-#Select A Columns
-col_option = st.selectbox("Select Column",("sepallength","sepalwidth","petallength","petalwidth","species"))
-if col_option == 'sepalwidth':
-  st.write(data['sepalwidth'])
-elif col_option == 'sepallength':
-  st.write(data['sepallength'])
-elif col_option == 'petalwidth':
-  st.write(data['petalwidth'])
-elif col_option == 'petallength':
-  st.write(data['petallength'])
-elif col_option == 'species':
-  st.write(data['species'])
-else:
-  st.write("Select Column")
-
-#Plot
-if st.checkbox("Show Bar Plot with Matplotlib"):
-  st.write(data.plot(kind='bar'))
-  st.pyplot()
-
-#Correlation
-if st.checkbox("Show Correlation Plot with Matplotlib"):
-  plt.matshow(data.corr())
-  st.pyplot()
-
-#Correlation
-if st.checkbox("Show Correlation Plot with Seaborn"):
-  st.write(sns.heatmap(data.corr()))
-  st.pyplot()
-
-#Group
-if st.checkbox("Show Bar Chart Plot"):
-  v_group = data.groupby('species)
-  st.bar_chart(v_group)
-
-#Group
-if st.checkbox("Show Line Plot"):
-  v_group = data.groupby('species)
-  st.Line_chart(data)
-
-#Group
-if st.checkbox("Show Area Chart Plot"):
-  v_group = data.groupby('species)
-  st.area_chart(v_group)
-
-
-#Images
-@st.cache 
-def load_image(img):
-  im = Image.open(os.path.join(img))
-  return im
-                         
-species_type=st.radio("Select Species Type",("setosa","virginica","versicolor"))
-if species_type == 'setosa':
-  st.text("Showing Setosa Species")
-  st.image(load_image('imgs/Iris_setosa.jpg'))
-
-if species_type == 'versicolor':
-  st.text("Showing versicolor Species")
-  st.image(load_image('imgs/Iris_versicolor.jpg'))
-
-if species_type == 'virginica':
-  st.text("Showing virginica Species")
-  st.image(load_image('imgs/Iris_virginica.jpg'))
-
-#Show Image
-if st.checkbox("Show/Hide Image"):
-  my_image = load_image("Iris_setosa.jpg")
-  enh = ImageEnhance.contrast(my_image)
-  num = st.slider("Set Image Contrast", 1.0,4.0)
-  img_width = st.slider("set Image width", 300,500)
-  st.image(enh.enhance(num),width img_width)
-
-# About
-if st.button("About App"):
-  st.text("Iris EDA App")
-  st.text("Built with Streamlit")
-  st.text("Cudosto the Streamlit Team")
 
 
