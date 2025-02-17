77260c52ba093284ed395def05c337d9ec09cf91
Jordan-Tchuimo <jorditchuimo@gmail.com>
2025-02-17 08:49:43 +0100

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -8,17 +8,18 @@ from PIL import Image
 
 
 #title/text
-st.title("iris EDA App")
+st.title("Iris EDA App")
 st.text("Built with streamlit")
 
 #EDA
-my_dataset = 'iris.csv'
+my_dataset = 'Iris.csv'
 
 #Fxn to Load Dataset
 
 def explore_data(dataset):
-  df = pd.read_csv(os.path.join(dataset))
+  df = pd.read_csv("Iris.csv")  # Example, check this
   return df
+
 
 
 if st.checkbox ("Preview Dataset"):
